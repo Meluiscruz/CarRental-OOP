6409c22dfaf3106f10ca1986336ce8ceacc621a1
dirtyg2120 <zone2120vn@gmail.com>
2019-11-25 23:45:06 +0700

    Adding car fleet using vector, serviceFleet still in process...

--- Car_rent.cpp
+++ Car_rent.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
@@ -8,7 +10,9 @@ protected:
 	string ID;
 	string color;
 public:
+	int cost;
 	Vehicle(int _days, string _ID, string _color): days(_days), ID(_ID), color(_color) {};
+
 	virtual void Print2Console() = 0;
 	virtual void serviceEngine() = 0;
 	virtual void serviceTransmission() = 0;
@@ -19,12 +23,19 @@ public:
  *	
  */
 
+struct Record{
+	double mileage;
+	int engine_cond;
+	int tires_cond;
+};
+
 class Sedan : public Vehicle {
 protected:
 	double trunk_cap;
+	Record VehicleHistory;
 public:
-	int cost;
 	Sedan(int _days, string _ID, string _color, double _trunk_cap) : Vehicle(_days, _ID, _color), trunk_cap(_trunk_cap) {}
+
 	void Print2Console() {
 		cout << "Car Type: \t\tSedan" << endl;
 		cout << "ID: \t\t\t" << ID << endl;
@@ -32,16 +43,32 @@ public:
 		cout << "Trunk Capacity: \t" << trunk_cap << endl;
 		cout << "Rented Days: \t\t" << days << endl;
 		cost = days * 1000;
-		cout << "Price: \t\t" << cost << " (USD)" << endl;
+		cout << "Price: \t\t\t" << cost << " (USD)" << endl;
 	}
 	void serviceEngine() {
-
+		cout << endl;
+		int choice;
+		cout << "This is our Engine service. What do you want for your Sedan?" << endl;
+		cout << "(1.Oil change, 2.Minor, 3.Major)" << endl;
+		cout << "Your choice: ";
+		cin >> choice;
 	}
 	void serviceTransmission() {
+		cout << endl;
+		int choice;
+		cout << "This is our Transmission service. What do you want for your Sedan?" << endl;
+		cout << "(1.Fluid change, 2.Minor, 3.Overhaul)" << endl;
+		cout << "Your choice: ";
+		cin >> choice;
 
 	}
 	void serviceTires() {
-
+		cout << endl;
+		int choice;
+		cout << "This is our Tires service. What do you want for your Sedan?" << endl;
+		cout << "(1.Adjustment, 2.Replacement)" << endl;
+		cout << "Your choice: ";
+		cin >> choice;
 	}
 };
 
@@ -54,13 +81,14 @@ struct Dimension{
 class PickUp : public Vehicle {
 protected:
 	Dimension trunk_dim;
+	Record VehicleHistory;
 public:
-	int cost;
 	void GetDim(int _width, int _length, int _height) {
 		trunk_dim.width = _width;
 		trunk_dim.length = _length;
 		trunk_dim.height = _height;
 	}
+
 	PickUp(int _days, string _ID, string _color) : Vehicle(_days, _ID, _color) {}
 	void Print2Console() {
 		cout << "Car Type: \t\tPickUp" << endl;
@@ -69,8 +97,9 @@ public:
 		cout << "Trunk Dimension: \t" << trunk_dim.width << " x " << trunk_dim.length << " x " << trunk_dim.height << " (m)" << endl;
 		cout << "Rented Days: \t\t" << days << endl;
 		cost = days * 1500;
-		cout << "Price: \t\t" << cost << " (USD)" << endl;
+		cout << "Price: \t\t\t" << cost << " (USD)" << endl;
 	}
+
 	void serviceEngine() {
 
 	}
@@ -80,15 +109,13 @@ public:
 	void serviceTires() {
 
 	}
-
-
 };
 
 class SUV : public Vehicle {
 protected:
 	int seats;
+	Record VehicleHistory;
 public:
-	int cost;
 	bool isFourSeat() {
 		if(seats == 4)
 			return true;
@@ -105,7 +132,7 @@ public:
 		else cout << "No" << endl;
 		cout << "Rented Days: \t\t" << days << endl;
 		cost = days * 2000;
-		cout << "Price: \t\t" << cost << " (USD)" << endl;
+		cout << "Price: \t\t\t" << cost << " (USD)" << endl;
 	}
 	void serviceEngine() {
 
@@ -119,26 +146,19 @@ public:
 
 };
 
-struct Record{
-	double mileage;
-	int engine_cond;
-	int tires_cond;
-};
-
 class CarRentalManagement{
 protected:
 	string name;
 	int age;
 	int identityNumber;
 	int PhoneNumber;
-	int carType;
-	int days;
-	string ID;
-	string color;
 	int moreCar;
-	CarRentalManagement* carArr;
+	vector <Vehicle*> Contracts;
 	int TotalPrice;
-	Record VehicleHistory;
+	//
+	double mileage;
+	int engine_cond;
+	int tires_cond;
 public:
 	void GetInfo() {
 		cout << "Your fullname: ";
@@ -150,25 +170,44 @@ public:
 		cout << "Phone number: ";
 		cin >> PhoneNumber;
 	}
-	void GetRentedInfo() {
-		cout << "Choose the type of car you want to rent: 1.Sedan  2.PickUp  3.SUV" << endl;
-		cout << "Your choice: "; 
-		cin >> carType;
-		cout << "How many days you rented: ";
-		cin >> days;
-		cout << "Car's ID in the database: ";
-		cin >> ID;
-		cout << "Choose color: ";
-		cin >> color;
-	}
-	void addCar() {
+
+
+	void AskforAmount() {
 		cout << "How many car you want to rent? : ";
 		cin >> moreCar;
-		carArr = new CarRentalManagement[moreCar];
+
+		int carType;
+		int _days;
+		string _ID;
+		string _color;
 		for(int i = 0; i < moreCar; i++) {
-			carArr[i].GetRentedInfo();
+			cout << "Choose the type of car you want to rent: 1.Sedan  2.PickUp  3.SUV" << endl;
+			cout << "Your choice: "; 
+			cin >> carType;
+			cout << "How many days you rented: ";
+			cin >> _days;
+			cout << "Car's ID in the database: ";
+			cin >> _ID;
+			cout << "Choose color: ";
+			cin >> _color;
+			
+			if(carType == 1) {
+				Vehicle* temp = new Sedan(_days, _ID, _color, 10);
+				Contracts.push_back(temp);
+			}
+			else if(carType == 2) {
+				Vehicle* temp = new PickUp(_days, _ID, _color);
+				Contracts.push_back(temp);
+			}
+			else if(carType == 3) {
+				Vehicle* temp = new SUV(_days, _ID, _color, 4);
+				Contracts.push_back(temp);
+			}
+
+			
 		}
 	}
+
 	void PrintBill() {
 		TotalPrice = 0;
 		cout << "------------------------------------" << endl;
@@ -177,32 +216,72 @@ public:
 		cout << "Age: \t\t\t" << age << endl;
 		cout << "Identity number: \t" << identityNumber << endl;
 		cout << "Phone number: \t\t" << PhoneNumber << endl;
-		cout << "Number of cars rented: " << moreCar + 1 << endl;
-		cout << "..." <<endl;
-
+		cout << "Number of cars rented: \t" << moreCar + 1 << endl;
+		cout << "..." << endl;
 		for(int i = 0; i < moreCar; i++) {
-			if(carArr[i].carType == 1) {
-				int trunk_cap = 10;
-				Sedan myCar(carArr[i].days, carArr[i].ID, carArr[i].color, trunk_cap);
-				myCar.Print2Console();
-				TotalPrice += myCar.cost;
-			} else if(carArr[i].carType == 2) {
-				PickUp myCar(carArr[i].days, carArr[i].ID, carArr[i].color);
-				myCar.GetDim(1, 2, 3);
-				myCar.Print2Console();
-				TotalPrice += myCar.cost;
-			} else if(carArr[i].carType == 3) {
-				SUV myCar(carArr[i].days, carArr[i].ID, carArr[i].color, 4);
-				myCar.Print2Console();
-				TotalPrice += myCar.cost;
-			}
-		cout << endl;
+			Contracts[i]->Print2Console();
+			TotalPrice += Contracts[i]->cost;
 		}
+		
 		cout << "Total Price: \t\t" << TotalPrice << " (USD)" << endl;
 		cout << "------------------------------------" << endl;
 	}
+
+	
+
 	void ServiceFleet() {
-		
+		cout << "      Car Service       " << endl;
+		cout << "Tell us about your car's status: " << endl;
+		for(int i = 0; i < moreCar; i++) {
+			cout << "1.Bad  2.Normal  3.Good" << endl;
+			cout << "Your car's tires condition: ";
+			cin >> tires_cond;
+			cout << "Your car's engine condition: ";
+			cin >> engine_cond;
+			cout << "Travled distance (miles): ";
+			cin >> mileage;
+
+			// TIRES' CONDITION
+			cout << endl;
+			if(tires_cond == 3)
+				cout << "There's nothing of your cars' tires need to be adjusted" << endl;
+			else if(tires_cond == 2) {
+				cout << "Do you want to adjust or replace something of your cars' tires? 1.Yes  2.No" << endl;
+				cout << "Your choice: ";
+				int yesno; cin >> yesno;
+				if(yesno == 1) {
+					Contracts[i]->serviceTires();
+				}
+				else cout << "OK, bye!!!" << endl;
+			}
+			else {
+				cout << "Your tires must be adjust or replace for the safety!!!" << endl;
+				Contracts[i]->serviceTires();
+			}
+
+			// ENGINE'S CONDITION
+			cout << endl;
+			if(engine_cond == 3)
+				cout << "There's nothing of your car's engine need to be adjusted" << endl;
+			else if(engine_cond == 2) {
+				cout << "Do you want to adjust or replace something of your car's engine? 1.Yes  2.No" << endl;
+				cout << "Your choice: ";
+				int yesno; cin >> yesno;
+				if(yesno == 1) {
+					Contracts[i]->serviceEngine();
+				}
+				else cout << "OK, bye!!!" << endl;
+			}
+			else {
+				cout << "Your car's engine must be adjust or replace for the safety!!!" << endl;
+				Contracts[i]->serviceEngine();
+			}
+			//	TRAVELED DISTANCE
+			cout << endl;
+			if(mileage > 3000) 
+				Contracts[i]->serviceTransmission();
+			else cout << "Your car is still good in transmission, comeback later" << endl;
+		}
 	}
 };
 
@@ -213,9 +292,10 @@ public:
 
 int main(){
 	CarRentalManagement a;
-	a.GetInfo();
-	a.addCar();
-	a.PrintBill();
+	//a.GetInfo();
+	a.AskforAmount();
+	//a.PrintBill();
+	a.ServiceFleet();
 
 	return 0;
 }
